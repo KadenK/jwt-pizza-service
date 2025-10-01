da87e4ac41005056c43875e0cd7fa7aadd88a267
Kaden Keep <kaden.keep@gmail.com>
2025-09-30 21:52:56 -0600

    add reference docs and userRouter tests

---
+++ src/routes/userRouter.test.js
@@ -0,0 +1,51 @@
+const request = require("supertest");
+const app = require("../service");
+
+let authToken;
+let userId;
+
+beforeAll(async () => {
+  const registerUserResp = await request(app).post("/api/auth").send({
+    name: "Test User",
+    email: "test@example.com",
+    password: "password123",
+  });
+
+  authToken = registerUserResp.body.token;
+  userId = registerUserResp.body.user.id;
+});
+
+test("Get authenticated user", async () => {
+  const response = await request(app)
+    .get("/api/user/me")
+    .set("Authorization", `Bearer ${authToken}`);
+
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual(
+    expect.objectContaining({
+      id: userId,
+      name: "Test User",
+      email: "test@example.com",
+    })
+  );
+});
+
+test("Update user", async () => {
+  const response = await request(app)
+    .put(`/api/user/${userId}`)
+    .send({
+      name: "Updated User",
+      email: "updated@example.com",
+    })
+    .set("Authorization", `Bearer ${authToken}`);
+
+  expect(response.status).toBe(200);
+  expect(response.body).toHaveProperty("user");
+  expect(response.body.user).toEqual(
+    expect.objectContaining({
+      id: userId,
+      name: "Updated User",
+      email: "updated@example.com",
+    })
+  );
+});
